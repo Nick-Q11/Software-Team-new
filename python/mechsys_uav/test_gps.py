6c6dc0a6eca1167b09d4a236443b57f3c3d9b222
Nick-Q11 <nick.zeh@gmx.net>
2026-06-18 11:28:27 +0200

    mavproxy

--- python/mechsys_uav/test_gps.py
+++ python/mechsys_uav/test_gps.py
@@ -1,23 +1,31 @@
 import asyncio
 from mechsys_uav import UAV
+import json
+import asyncio
+from importlib.resources import files
+from shapely.geometry import Point, Polygon
+from mavsdk import System
+from mavsdk.telemetry import FlightMode
+
 
 
 async def main():
 
     uav = await UAV.connect(
         use_sim=False,
-        serial_device="/dev/cu.usbmodem01"
+        serial_device="/dev/ttyACM0"
     )
 
     await asyncio.sleep(2)
+    try:
+        while True:
+            latitude,longitude,relative_altitude = uav.get_position()
 
-    while True:
-        latitude,longitude,relative_altitude = uav.get_position()
+            print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {relative_altitude}")
 
-        print(f"Latitude: {latitude}, Longitude: {longitude}, Altitude: {relative_altitude}")
-
-        await asyncio.sleep(1)
-
+            await asyncio.sleep(5)
+    except KeyboardInterrupt:
+        print("GPS beendet")
 if __name__ == '__main__':
     asyncio.run(main())
 
