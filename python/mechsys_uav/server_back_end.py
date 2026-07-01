27ccbab2199223c5ddb7c18ef4d1b4a3effb2048
DoldiePeter <nopedinopenope@gmx.de>
2026-07-02 00:54:31 +0200

    I_did_something

--- python/mechsys_uav/server_back_end.py
+++ python/mechsys_uav/server_back_end.py
@@ -2,52 +2,41 @@ from flask import Flask, render_template, request, jsonify
 from mechsys_uav import UAV
 
 app = Flask(__name__)
-
+    
 # Store the latest coordinates
 current_location = {
     'latitude': 0.0,
     'longitude': 0.0,
 }
 
+
+def update_location(latitude=None, longitude=None):
+    if latitude is not None:
+        current_location['latitude'] = float(latitude)
+    if longitude is not None:
+        current_location['longitude'] = float(longitude)
+
+
 @app.route('/')
 def index():
     """Serve the main webpage"""
     return render_template('index.html')
 
-
-def update_location(long =0.0, lat=0.0):
-    
-    global current_location
-    data = request.json
-    current_location['latitude'] = data.get('latitude', lat)
-    current_location['longitude'] = data.get('longitude', long)
-
-    print(f"Location updated: {current_location}")
-    return jsonify({'status': 'success'})
-
-def auto_update_location(uav: UAV):
-    """Update the location based on the UAV's current position"""
-    global current_location
-    data = request.json
-    lat, long, alt = uav.get_position()
-        
-    current_location['latitude'] = data.get('latitude', lat)
-    current_location['longitude'] = data.get('longitude', long)
-
-    print(f"Location updated: {current_location}")
-    return jsonify({'status': 'success'})
-
 @app.route('/get_location')
 def get_location():
+    
     """Endpoint for webpage to get current coordinates"""
     return jsonify(current_location)
 
 def main():
+    
+    index()
+
+def test():   
     long = 49.12
     lat = 11.12
     update_location(long, lat)
-    
-
 if __name__ == '__main__':
     # Run on all network interfaces, port 5000
+    test()
     app.run(host='0.0.0.0', port=5000, debug=False)
